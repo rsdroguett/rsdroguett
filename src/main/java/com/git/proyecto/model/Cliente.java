130cf0ecf6c4bf68b5f825fbd28cc5d6dd75e008
Dafor <Dafor@DESKTOP-AMDOG66>
2021-08-26 22:34:47 -0400

    Actualización Dafo

---
+++ src/main/java/com/git/proyecto/model/Cliente.java
@@ -0,0 +1,15 @@
+package com.git.proyecto.model;
+
+public class Cliente {
+
+	private String nombreString;
+
+	public String getNombreString() {
+		return nombreString;
+	}
+
+	public void setNombreString(String nombreString) {
+		this.nombreString = nombreString;
+	}
+	
+}
