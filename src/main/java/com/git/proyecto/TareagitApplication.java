2bfa2beae78382096cee8adcc2cc77b2f6383943
rosad <rosad@LAPTOP-21FHHL60>
2021-08-26 21:43:42 -0400

    primer commit

---
+++ src/main/java/com/git/proyecto/TareagitApplication.java
@@ -0,0 +1,13 @@
+package com.git.proyecto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TareagitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TareagitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/git/proyecto/TareagitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.git.proyecto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TareagitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
