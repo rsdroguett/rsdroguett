4e3c604ecebec1186f53966f2ff42a57adecfa23
Dafor <Dafor@DESKTOP-AMDOG66>
2021-08-26 22:45:56 -0400

    Creación de boleta

---
+++ src/main/java/com/git/proyecto/model/Boleta.java
@@ -0,0 +1,16 @@
+package com.git.proyecto.model;
+
+public class Boleta {
+
+	private String estoEsUnaBoletaString;
+
+	public String getEstoEsUnaBoletaString() {
+		return estoEsUnaBoletaString;
+	}
+
+	public void setEstoEsUnaBoletaString(String estoEsUnaBoletaString) {
+		this.estoEsUnaBoletaString = estoEsUnaBoletaString;
+	}
+	
+	
+}
